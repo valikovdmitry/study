6b1fc702a11e6c636ffe3c34680930a2dfeabf2e
Dmitry Valikov <dmitryvalikov@MacBook-Pro.local>
2022-03-04 14:23:16 +0300

    Strings and types

--- main.py
+++ main.py
@@ -1,17 +1,50 @@
-# if statement
+c = 'Sample '
+b = '\n \t text'
+d = '''
+Текст
+который
+будет
+на нескольих
+строках
+который'''
+e = '''Простой {1} текст, которыя я придумал {1} {0}'''
+f = f'Простой {b}'
 
-a = 5
-b = 6
 
 
-d = True
-e = False
-f = a == b
+#print('который' in d)
+#print(d.find('который'))
+#print(d.replace('который', 'каторый'))
+
+# приведение типов
+
+# int
+r = int(5)
+#print(float(r))
+#print(str(r))
+#print(bool(r))
+
+# float
+u = float(5.6)
+#print(u)
+#print(int(u))
+#print(str(u))
+#print(bool(u))
+
+# string
+new_string = '5.6'
+#print(new_string)
+#print(int(new_string))
+#print(float(new_string))
+#print(bool(new_string))
+#print(int(float(new_string)))
 
 
-if f:
-    print(d)
-elif e:
-    print(d)
-else:
-    print("hui")
+# провека типа переменной
+#print(type(u))
+#print(type(new_string))
+#print(type(r))
+
+#print(isinstance(u, float))
+
+

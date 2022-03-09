b9ba5b9898da0273f9801a7f21069997b72d3ee1
Dmitry Valikov <dmitryvalikov@MacBook-Pro.local>
2022-03-10 02:21:49 +0300

    Homework for Data Structure

--- homework_data_structure.py
+++ homework_data_structure.py
@@ -18,3 +18,38 @@ find(lst)
 find(lst2)
 
 
+
+
+# 2. На входе список из любых типов переменных. Задача удалиить из списка все с типом str.
+
+lst = [1, 0.5, 'sring', 2, 0.7, 'word', 7, 3.5, 'sea']
+lst2 = [2, 1.5, 'star', 3, 8.7, 'work', 9, 4.5, 'tea']
+
+def remove(x, y):
+    print('\n', x)
+    for value in x:
+        if (isinstance(value, y)) == True:
+            x.remove(value)
+    print('\n \t After remove str:', '\n', '\t', x, '\n \n')
+
+remove(lst, str)
+remove(lst2, str)
+
+
+
+
+# 3. Функция которая находит индекс максимального числа
+
+lst = [1, 2, 3, 4, 12, 5, 6, 7, 17, 9]
+lst2 = []
+for i in range(21, 41):
+    lst2.append(i)
+
+result = []
+
+def findmax(x):
+    print('\nСамое большое число:', max(x), '.')
+    print('Оно находится на', x.index(max(x)), 'месте в массиве.')
+
+findmax(lst)
+findmax(lst2)

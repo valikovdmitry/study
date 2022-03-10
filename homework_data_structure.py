5d97b2645b39eaf401f48ed9a3209b3a4bd73a68
dmitryvalikov <valikov.dmitry@gmail.com>
2022-03-10 07:05:37 +0300

    Homework

--- homework_data_structure.py
+++ homework_data_structure.py
@@ -25,15 +25,15 @@ find(lst2)
 lst = [1, 0.5, 'sring', 2, 0.7, 'word', 7, 3.5, 'sea']
 lst2 = [2, 1.5, 'star', 3, 8.7, 'work', 9, 4.5, 'tea']
 
-def remove(x, y):
+def remove(x, y=str):
     print('\n', x)
     for value in x:
         if (isinstance(value, y)) == True:
             x.remove(value)
     print('\n \t After remove str:', '\n', '\t', x, '\n \n')
 
-remove(lst, str)
-remove(lst2, str)
+remove(lst)
+remove(lst2)
 
 
 
